a8226b372e5bf534f9d464459210a92505470f07
redrza8118 <cosmicosyy@gmail.com>
2026-09-18 06:41:13 -0600

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,456 @@
+const cards = [
+    {
+        name: "Kyrie Irving",
+        image: "images/KyrieIrvingBasketbot.png",
+        rarity: "SPECIAL",
+        shooting: 91,
+        finishing: 93,
+        defending: 82,
+        playmaking: 96,
+        height: "6'2\"",
+        position: ["PG", "SG"],
+        marketEstimate: "₳ 70K-80K",
+
+        traits: [
+            {
+                name: "Area Architect",
+                level: 2
+            },
+            {
+                name: "Deep Volume",
+                level: 2
+            },
+            {
+                name: "Hog",
+                level: 1
+            },
+            {
+                name: "Icy",
+                level: 1
+            },
+            {
+                name: "Midrange Maestro",
+                level: 2
+            },
+            {
+                name: "Unfazed",
+                level: 1
+            }
+        ]
+    },
+
+    {
+        name: "Jose Alvarado",
+        image: "images/JoseAlvaradoBasketbot.png",
+        rarity: "Finals '26",
+        shooting: 84,
+        finishing: 82,
+        defending: 92,
+        playmaking: 87,
+        height: "6'0\"",
+        position: ["PG"],
+        marketEstimate: "₳ 30K-50K",
+
+        traits: [
+            {
+                name: "Clamp Time",
+                level: 2
+            },
+            {
+                name: "Icy",
+                level: 1
+            },
+            {
+                name: "Selfless",
+                level: 1
+            },
+            {
+                name: "Solid Stats",
+                level: 1
+            },
+            {
+                name: "Thief",
+                level: 3
+            }
+        ]
+    },
+
+    {
+        name: "Isaiah Hartenstein",
+        image: "images/IsaiahHartensteinBasketbot.png",
+        rarity: "FROST V4",
+        shooting: 78,
+        finishing: 91,
+        defending: 91,
+        playmaking: 81,
+        height: "7'0\"",
+        position: ["C"],
+        marketEstimate: "₳ 150K-200K",
+
+        traits: [
+            {
+                name: "Clamp Time",
+                level: 1
+            },
+            {
+                name: "Dynamic Defender",
+                level: 1
+            },
+            {
+                name: "Ferocious Finisher",
+                level: 2
+            },
+            {
+                name: "Helping Hand",
+                level: 1
+            },
+            {
+                name: "Midrange Maestro",
+                level: 1
+            },
+            {
+                name: "Post Punisher",
+                level: 1
+            },
+            {
+                name: "Resilient Rebounder",
+                level: 3
+            },
+            {
+                name: "Shot Stopper",
+                level: 1
+            },
+            {
+                name: "Solid Stats",
+                level: 1
+            },
+            {
+                name: "Thief",
+                level: 1
+            }
+        ]
+    },
+
+    {
+        name: "Scottie Barnes",
+        image: "images/ScottieBarnesBasketbot.png",
+        rarity: "FROST V4",
+        shooting: 85,
+        finishing: 92,
+        defending: 91,
+        playmaking: 91,
+        height: "6'7\"",
+        position: ["PF", "SF"],
+        marketEstimate: "₳ 800K-900K",
+
+        traits: [
+            {
+                name: "Area Architect",
+                level: 1
+            },
+            {
+                name: "Clamp Time",
+                level: 1
+            },
+            {
+                name: "Dynamic Defender",
+                level: 1
+            },
+            {
+                name: "Ferocious Finisher",
+                level: 2
+            },
+            {
+                name: "Helping Hand",
+                level: 1
+            },
+            {
+                name: "Icy",
+                level: 1
+            },
+            {
+                name: "Midrange Maestro",
+                level: 1
+            },
+            {
+                name: "Post Punisher",
+                level: 1
+            },
+            {
+                name: "Resilient Rebounder",
+                level: 2
+            },
+            {
+                name: "Shot Stopper",
+                level: 1
+            },
+            {
+                name: "Solid Stats",
+                level: 1
+            },
+            {
+                name: "Thief",
+                level: 1
+            },
+            {
+                name: "Unfazed",
+                level: 1
+            }
+        ]
+    },
+
+    {
+        name: "Alex Sarr",
+        image: "images/AlexSarrBasketbot.png",
+        rarity: "FUTURE STAR V3",
+        shooting: 86,
+        finishing: 91,
+        defending: 93,
+        playmaking: 83,
+        height: "7'0\"",
+        position: ["C", "PF"],
+        marketEstimate: "₳ 550K-650K",
+
+        traits: [
+            {
+                name: "Area Architect",
+                level: 1
+            },
+            {
+                name: "Clamp Time",
+                level: 2
+            },
+            {
+                name: "Deep Volume",
+                level: 1
+            },
+            {
+                name: "Dynamic Defender",
+                level: 1
+            },
+            {
+                name: "Ferocious Finisher",
+                level: 2
+            },
+            {
+                name: "Midrange Maestro",
+                level: 1
+            },
+            {
+                name: "Post Punisher",
+                level: 1
+            },
+            {
+                name: "Resilient Rebounder",
+                level: 2
+            },
+            {
+                name: "Shot Stopper",
+                level: 2
+            },
+            {
+                name: "Solid Stats",
+                level: 1
+            },
+            {
+                name: "Thief",
+                level: 1
+            },
+            {
+                name: "Unfazed",
+                level: 1
+            }
+        ]
+    },
+
+    {
+        name: "Kawhi Leonard",
+        image: "images/KawhiLeonardBasketbot.png",
+        rarity: "SPECIAL",
+        shooting: 89,
+        finishing: 90,
+        defending: 96,
+        playmaking: 85,
+        height: "6'7\"",
+        position: ["SF", "SG"],
+        marketEstimate: "₳ 80K-100K",
+
+        traits: [
+            {
+                name: "Area Architect",
+                level: 1
+            },
+            {
+                name: "Clamp Time",
+                level: 2
+            },
+            {
+                name: "Dynamic Defender",
+                level: 2
+            },
+            {
+                name: "Ferocious Finisher",
+                level: 1
+            },
+            {
+                name: "Icy",
+                level: 2
+            },
+            {
+                name: "IQ",
+                level: 1
+            },
+            {
+                name: "Midrange Maestro",
+                level: 1
+            },
+            {
+                name: "Thief",
+                level: 2
+            }
+        ]
+    },
+
+    {
+        name: "Chris Paul",
+        image: "images/CP3Basketbot.png",
+        rarity: "RARE",
+        shooting: 88,
+        finishing: 82,
+        defending: 88,
+        playmaking: 99,
+        height: "6'0\"",
+        position: ["PG"],
+        marketEstimate: "₳ 7K",
+
+        traits: [
+            {
+                name: "Clamp Time",
+                level: 1
+            },
+            {
+                name: "Helping Hand",
+                level: 2
+            },
+            {
+                name: "IQ",
+                level: 2
+            },
+            {
+                name: "Midrange Maestro",
+                level: 1
+            },
+            {
+                name: "Selfless",
+                level: 1
+            },
+            {
+                name: "Thief",
+                level: 1
+            }
+        ]
+    },
+
+    {
+        name: "Russell Westbrook",
+        image: "images/BrodieBasketbot.png",
+        rarity: "RARE",
+        shooting: 79,
+        finishing: 93,
+        defending: 88,
+        playmaking: 89,
+        height: "6'3\"",
+        position: ["PG"],
+        marketEstimate: "Unknown",
+
+        traits: [
+            {
+                name: "Ferocious Finisher",
+                level: 1
+            },
+            {
+                name: "Hog",
+                level: 1
+            },
+            {
+                name: "Resilient Rebounder",
+                level: 1
+            }
+        ]
+    },
+
+    {
+        name: "Ben Simmons",
+        image: "images/BenSimmonsBasketbot.png",
+        rarity: "RARE",
+        shooting: 41,
+        finishing: 93,
+        defending: 90,
+        playmaking: 83,
+        height: "6'11\"",
+        position: ["PF", "PG"],
+        marketEstimate: "₳ 10K",
+
+        traits: [
+            {
+                name: "Clamp Time",
+                level: 1
+            },
+            {
+                name: "Dynamic Defender",
+                level: 1
+            },
+            {
+                name: "Ferocious Finisher",
+                level: 1
+            },
+            {
+                name: "Resilient Rebounder",
+                level: 1
+            },
+            {
+                name: "Thief",
+                level: 1
+            }
+        ]
+    },
+
+    {
+        name: "Rudy Gobert",
+        image: "images/RudyGobertBasketbot.png",
+        rarity: "RARE",
+        shooting: 51,
+        finishing: 89,
+        defending: 99,
+        playmaking: 78,
+        height: "7'1\"",
+        position: ["C"],
+        marketEstimate: "₳ 10K",
+
+        traits: [
+            {
+                name: "Dynamic Defender",
+                level: 1
+            },
+            {
+                name: "Ferocious Finisher",
+                level: 1
+            },
+            {
+                name: "Resilient Rebounder",
+                level: 2
+            },
+            {
+                name: "Selfless",
+                level: 1
+            },
+            {
+                name: "Shot Stopper",
+                level: 2
+            }
+        ]
+    }
+];
+
+console.log("cards.js loaded successfully");
+console.log(cards);
